988e3816bb1653cda66da7e53099c6cea93204b5
txger. <simaocampanico.dev@gmail.com>
2025-05-02 10:21:31 +0100

    Adding PlayerManager1 with the class Player.cs

---
+++ PlayerManager1/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace PlayerManager1
+{
+    public class Player
+    {
+        
+        public string Name { get; }
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        
+    }
+}
