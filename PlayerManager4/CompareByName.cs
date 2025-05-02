a0c3dd6ce33855fa978021a3e86dd8d229a65559
txger. <simaocampanico.dev@gmail.com>
2025-05-02 10:46:24 +0100

    Creating PlayerManager4, adding it to the solution and creating CompareByName.cs, Player.cs and using Program.cs from the other Managers

---
+++ PlayerManager4/CompareByName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace PlayerManager4
+{
+    public class CompareByName : IComparer
+    {
+
+        private bool _ascending;
+        public CompareByName(bool ascending)
+        {
+            _ascending = ascending;
+        }
+
+        public int Compare(object x, object y)
+        {
+            if (!(x is string xx) || !(y is string yy)) return -1;
+            if (_ascending) return xx.CompareTo(yy);
+            return yy.CompareTo(xx);
+        }
+    }
+}
---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PlayerManager4
+{
+    public class Player : IComparable
+    {
+        
+        public string Name { get; }
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null || !(obj is Player other)) return 1;
+            return other.Score - this.Score;
+        }
+        
+    }
+}
